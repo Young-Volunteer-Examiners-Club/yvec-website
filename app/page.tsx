1b224447b1ff8273e05e554d9dec7711b8ce1b9e
SpaceXAce11 <95454375+SpaceXAce11@users.noreply.github.com>
2026-08-20 17:40:51 -0400

    Updated some things!

--- app/page.tsx
+++ app/page.tsx
@@ -259,8 +259,8 @@ export default function Home() {
       </p>
       <p className="info">
         <br /> • "Membership may not be denied or terminated because of race, creed, color, religion, gender, sexual orientation, political affiliation, marital status, disability or any other reason that would be biased or prejudicial" - YVEC Constituion and Bylaws
-        <br /> • We proudly support any and all members of the LGBTQIA+ community, people of color, any and all religions, the disabled, and ohter marginilaized groups!
-        <br /> • All are welcome. If you think otherwise, you aren't! Bigotry is not allowed and will result in immediate termination! This includes but is not limited to: Racisim, Homophobia, Transphobia, Xenophobia, and Classism.
+        <br /><br /> • We proudly support people of the LGBTQIA+ community, people of color, members of any and all religions, the disabled, and people apart of marginilaized groups!
+        <br /><br /> • All are welcome. If you think otherwise, you aren't! Bigotry is not allowed and will result in immediate termination! This includes but is not limited to: Racisim, Homophobia, Transphobia, Xenophobia, and Classism.
       </p>
       
       <h1 className="section-title">Contact</h1>
